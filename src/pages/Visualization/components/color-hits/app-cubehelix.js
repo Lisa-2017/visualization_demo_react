139fbfc6731540c8b126d52255e538631b63ffde
wb-lx652828 <wb-lx652828@alibaba-inc.com>
2021-02-25 18:13:44 +0800

    build | 安装cubehelix依赖。测试cubehelix的导入方式（2种）

--- src/pages/Visualization/components/color-hits/app-cubehelix.js
+++ src/pages/Visualization/components/color-hits/app-cubehelix.js
@@ -1,4 +1,9 @@
-import { cubehelix } from "../../../../utils/common/lib/color/cubehelix/index.js";
+// import { cubehelix } from "../../../../utils/common/lib/color/cubehelix/index.js";
+import {cubehelix} from 'cubehelix';
+// 在.mjs中无法使用import导入npm 库中的模块，但在.js文件中正常。
+// 但是 在.mjs文件中 可以使用import导入本地模块文件。所以以上两种方式均可
+
+
 /**
  * 因为本案例中使用了requestAnimationFrame ，所以会出现闪烁的情况, 使用save方法保存canvas状态可解决
  * 闪烁原因：TODO暂无理论支持
@@ -7,12 +12,9 @@ import { cubehelix } from "../../../../utils/common/lib/color/cubehelix/index.js
 const InitCubeHelix = (t) => {
   const canvas = document.querySelector("#canvasCube");
   const ctx = canvas.getContext("2d");
-
   ctx.clearRect(0, 0, 512, 512);
 
   ctx.save()
-  ctx.translate(0, 0);
-  ctx.scale(1, 1);
   ctx.fillStyle=`rgb(256,256,256)`;
   ctx.fillText("CubeHelix的方式显示彩色rect", 180, 100);
   ctx.restore()
--- src/pages/Visualization/components/color-hits/index.js
+++ src/pages/Visualization/components/color-hits/index.js
@@ -1,6 +1,6 @@
 import InitRGB from "./app-rgb.mjs";
 import InitHSL from "./app-hsl.mjs";
 import InitLab from "./app-lab.mjs";
-import InitCubeHelix from "./app-cubehelix.mjs";
+import InitCubeHelix from "./app-cubehelix.js";
 
 export { InitRGB, InitHSL, InitCubeHelix, InitLab };
